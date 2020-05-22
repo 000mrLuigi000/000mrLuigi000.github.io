91b3b92e6d74429e7968132c7d7a3bcc60916037
Вячеслав Шахтаров <45519709+000mrLuigi000@users.noreply.github.com>
2020-05-22 22:06:49 +0300

    v0.1
    Добаил выбор города и всю верхнию плашку

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
-'use strict';
-/*
-Импортирую модули: фабрика елементов, фабрика персон, база данных, фабрику школы, руководителя.
- */
-import { FactoryElements } from '../js/component.js';
-import { PersonFactory } from '../js/personLib.js';
-import { DataBase } from '../js/db.js';
-import { SchoollFactory } from '../js/school.js';
-import { SuperVisor } from '../js/supervisor.js';
-/*
-Создаю экзкмляры.
-*/
-const factoryElements = new FactoryElements();
-const personFactory = new PersonFactory();
-const schoollFactory = new SchoollFactory();
-const db = new DataBase();
-const supervisor = new SuperVisor();
-/*
-Отдаю руководителю  фабрику елементв,  фабрику школы, базу данных, для дальнейшей работы с ними.
-*/
-supervisor.addElement(schoollFactory, 'scholl');
-supervisor.addElement(db, 'db');
-supervisor.addElement(factoryElements, 'factoryElements');
-/*
-Создаю элементы шапку и хранилище контента.
-*/
-factoryElements.createElement('header', document.body, { id: 'header' }, supervisor);
-factoryElements.createElement('content', document.body, { id: 'content' }, supervisor);
-/*
-Подгружая данные с базы данных создаю карточки персон.
-*/
-db.getStudentList.forEach((property) => {
-    const student = personFactory.createPerson(property);
-    schoollFactory.addStudent(student);
-    factoryElements.createElement('person', document.getElementById('student-container'), student, supervisor);
+const buttonCompany = document.getElementById('buttonCompany');
+const buttonSupport = document.getElementById('buttonSupport');
+const buttonCity = document.getElementById('buttonCity');
+const buttonClose = document.getElementById('buttonClose');
+const cityName = document.getElementsByName('cityName')
+const tableCompany = document.getElementById('tableCompany');
+const tableSupport = document.getElementById('tableSupport');
+const formCity = document.getElementById('formCity');
+const titleCityName = document.getElementById('titleCityName');
+
+titleCityName.textContent = localStorage.getItem('cityName') || 'Санкт-Петербург';
+
+buttonCompany.addEventListener('mouseenter', () => {
+    tableCompany.classList.add('head__flexContainer__selector__tableCompany_action');
 });
-db.getTeacherList.forEach((property) => {
-    const teacher = personFactory.createPerson(property);
-    schoollFactory.addTeacher(teacher);
-    factoryElements.createElement('person', document.getElementById('teacher-container'), teacher, supervisor);
+buttonCompany.addEventListener('mouseleave', () => {
+    tableCompany.classList.remove('head__flexContainer__selector__tableCompany_action');
 });
+buttonSupport.addEventListener('mouseenter', () => {
+    tableSupport.classList.add('head__flexContainer__selector__tableSupport_action');
+});
+buttonSupport.addEventListener('mouseleave', () => {
+    tableSupport.classList.remove('head__flexContainer__selector__tableSupport_action');
+});
+buttonCity.addEventListener('click', () => {
+    formCity.classList.add('head__flexContainer__formCity_action');
+});
+buttonClose.addEventListener('click', () => {
+    formCity.classList.remove('head__flexContainer__formCity_action');
+});
+
+cityName.forEach((but)=>{
+    but.addEventListener('click', ()=>{
+        formCity['curCityName'] = but.value;
+    });
+});
+formCity.addEventListener('submit', (event)=>{
+   // event.preventDefault();
+    localStorage.setItem('cityName',formCity['curCityName'])
+});
+
