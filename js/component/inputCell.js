a25ab8b05322cecb0412aabb02287550f79f1d8d
mrLuigi <ddd-dddu@yandex.ru>
2020-07-01 15:12:57 +0300

    tets3 0.1

--- js/component/inputCell.js
+++ js/component/inputCell.js
@@ -1,22 +1,23 @@
 'use strict';
-/**
- * Класс компонента InputCell.
- * Служит созданию нажимной клетки.
- */
-export class InputCell extends Inferno.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            img: '/',
-            func: props.func ?? undefined //Функция срабаотываемая при нажатии
-        };
-    }
 
-    render() {
-        return Inferno.createElement('td', {
-            className: this.props.clas,
-            onClick: (this.state.func) ? ()=>{this.state.func([this.props.i,this.props.j]);} : '',
-            style: {'background-image':`url(./img/${this.state.img})`}
-        }, this.props.cont ?? '');
-    }
+let createVNode = Inferno.createVNode;
+let createFragment = Inferno.createFragment;
+export default class InputCell extends Inferno.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageName: props.imageName ?? '',
+      whoWin: props.whoWin ?? ''
+    };
+  }
+
+  render() {
+    return createVNode(1, "div", "main__arena__inputCell", null, 1, {
+      "onClick": () => {
+        this.props.click(this);
+      },
+      "style": `background-image: url(./img/${this.state.imageName}); background-color: ${this.state.whoWin};`
+    });
+  }
+
 }
